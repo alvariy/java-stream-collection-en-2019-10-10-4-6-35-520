5af453eef708036307310a0ab45f9642ffca5a8a
IYAN OLIVER ALVAREZ (PLAN-IRIS-ISD-OOCLL/MNL) <ALVARIY@OOCL.COM>
2019-10-11 08:30:46 +0800

    Pass Flatten and Filter Tests

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -2,7 +2,11 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Flaten {
 
@@ -12,10 +16,32 @@ public class Flaten {
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+
+        List<Integer> oneDArray = new ArrayList<>();
+
+        for(int i = 0; i<array.length; i++)
+        {
+            for(int j = 0; j< array[i].length; j++)
+            {
+                oneDArray.add(array[i][j]);
+            }
+        }
+
+        return oneDArray;
+//        throw new NotImplementedException();
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> oneDArray = new ArrayList<>();
+
+        for(int i = 0; i<array.length; i++)
+        {
+            for(int j = 0; j< array[i].length; j++)
+            {
+                oneDArray.add(array[i][j]);
+            }
+        }
+        
+        return oneDArray.stream().distinct().collect(Collectors.toList());
     }
 }
